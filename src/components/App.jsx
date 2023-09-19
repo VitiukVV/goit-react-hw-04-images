0d509d65e88c9a2b26750bdd4b21cf410516fee5
Volodymyr Vitiuk <v.v.endeavour@gmail.com>
2023-09-19 15:42:25 +0300

    moved the logic to the App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,27 +2,86 @@ import React, { Component } from 'react';
 import { Container } from './App.styled';
 import { Searchbar } from './Searchbar/Searchbar';
 import { ImageGallery } from './ImageGallery/ImageGallery';
-
-// import PropTypes from 'prop-types';
+import { LoadMoreBtn } from './LoadMoreBtn/LoadMoreBtn';
+import { Loader } from './Loader/Loader';
+import { PixabayAPIRequest } from './PixabayAPI/API';
 
 export class App extends Component {
   state = {
-    searchValue: '',
+    searchRequest: '',
+    searchValue: [],
+    loading: false,
+    error: null,
+    page: 1,
+    totalPage: 0,
   };
 
+  componentDidUpdate(prevProps, prevState) {
+    const prevSearch = prevState.searchRequest;
+    const nextSearch = this.state.searchRequest;
+    const { page } = this.state;
+    if (prevSearch !== nextSearch) {
+      this.firstSearch(nextSearch);
+    }
+    if (prevState.page !== page && page !== 1) {
+      this.loadMore(prevState, page, nextSearch);
+    }
+  }
+
   handleFormSubmit = seacrhParam => {
-    this.setState({ searchValue: seacrhParam });
+    this.setState({ searchRequest: seacrhParam });
+  };
+
+  firstSearch = async search => {
+    try {
+      this.setState({ loading: true, searchValue: [] });
+      const response = await PixabayAPIRequest(search, 1);
+      if (response.totalHits < 1) {
+        this.setState({ loading: false });
+        return alert(`${search} not found, Sorry!`);
+      }
+      this.setState({
+        searchValue: response.hits,
+        totalPage: Math.ceil(response.totalHits / 12),
+        page: 1,
+      });
+    } catch ({ message }) {
+      this.setState({ error: message });
+    } finally {
+      this.setState({ loading: false });
+    }
+  };
+
+  loadMore = async (prevState, page, nextSearch) => {
+    try {
+      this.setState({ loading: true });
+      const response = await PixabayAPIRequest(nextSearch, page);
+      this.setState(prevState => ({
+        searchValue: [...prevState.searchValue, ...response.hits],
+      }));
+    } catch ({ message }) {
+      this.setState({ error: message });
+    } finally {
+      this.setState({ loading: false });
+    }
+  };
+
+  onChangePage = () => {
+    this.setState(prevState => ({ page: prevState.page + 1 }));
   };
 
   render() {
-    const { searchValue } = this.state;
+    const { searchValue, totalPage, page, loading } = this.state;
+
     return (
       <Container>
         <Searchbar onSubmit={this.handleFormSubmit}></Searchbar>
         <ImageGallery searchValue={searchValue}></ImageGallery>
+        {loading && <Loader></Loader>}
+        {totalPage > 1 && page < totalPage && (
+          <LoadMoreBtn onClick={this.onChangePage} />
+        )}
       </Container>
     );
   }
 }
-
-// App.propTypes = {};
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,83 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { ImageGalleryList } from './ImageGallery.styled';
 import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
-import { PixabayAPIRequest } from 'components/PixabayAPI/API';
-import { Loader } from 'components/Loader/Loader';
-import { LoadMoreBtn } from 'components/LoadMoreBtn/LoadMoreBtn';
+
 import PropTypes from 'prop-types';
 
-export class ImageGallery extends Component {
-  state = {
-    searchValue: [],
-    loading: false,
-    error: null,
-    page: 1,
-    totalPage: 0,
-  };
-
-  async componentDidUpdate(prevProps, prevState) {
-    try {
-      const prevSearch = prevProps.searchValue;
-      const nextSearch = this.props.searchValue;
-      const { page } = this.state;
-      if (prevSearch !== nextSearch) {
-        this.firstSearch(nextSearch);
-      }
-      if (prevState.page !== page && page !== 1) {
-        this.loadMore(prevState, page, nextSearch);
-      }
-    } catch ({ message }) {
-      this.setState({ error: message });
-    }
-  }
-
-  firstSearch = async search => {
-    this.setState({ loading: true, searchValue: [] });
-    const response = await PixabayAPIRequest(search, 1);
-    if (response.totalHits < 1) {
-      this.setState({ loading: false });
-      return alert(`${search} not found, Sorry!`);
-    }
-    this.setState({
-      searchValue: response.hits,
-      totalPage: Math.ceil(response.totalHits / 12),
-      loading: false,
-      page: 1,
-    });
-  };
-
-  loadMore = async (prevState, page, nextSearch) => {
-    this.setState({ loading: true });
-    const response = await PixabayAPIRequest(nextSearch, page);
-    this.setState(prevState => ({
-      searchValue: [...prevState.searchValue, ...response.hits],
-    }));
-    this.setState({ loading: false });
-  };
-
-  onChangePage = () => {
-    this.setState(prevState => ({ page: prevState.page + 1 }));
-  };
-
-  render() {
-    const { searchValue, totalPage, page } = this.state;
-    return (
-      <>
-        <ImageGalleryList>
-          {searchValue &&
-            searchValue.map(card => (
-              <ImageGalleryItem key={card.id} item={card}></ImageGalleryItem>
-            ))}
-        </ImageGalleryList>
-        {this.state.loading && <Loader></Loader>}
-        {totalPage > 1 && page < totalPage && (
-          <LoadMoreBtn onClick={this.onChangePage} />
-        )}
-      </>
-    );
-  }
-}
+export const ImageGallery = ({ searchValue }) => {
+  return (
+    <ImageGalleryList>
+      {searchValue &&
+        searchValue.map(card => (
+          <ImageGalleryItem key={card.id} item={card}></ImageGalleryItem>
+        ))}
+    </ImageGalleryList>
+  );
+};
 
 ImageGallery.propTypes = {
-  searchValue: PropTypes.string.isRequired,
+  searchValue: PropTypes.array.isRequired,
 };
