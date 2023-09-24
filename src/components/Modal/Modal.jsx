3c7c357a2bedc820b31e7bb5b03630778e54b2ac
Volodymyr Vitiuk <v.v.endeavour@gmail.com>
2023-09-24 16:15:38 +0300

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -3,23 +3,26 @@ import { createPortal } from 'react-dom';
 import { ModalBackdrop, ModalContent } from './Modal.styled';
 import PropTypes from 'prop-types';
 import { useEffect } from 'react';
+import { useCallback } from 'react';
 
 const modalRoot = document.querySelector('#modal-root');
 
 export const Modal = ({ onClose, children }) => {
-  const handleKeyDown = e => {
-    if (e.code === 'Escape' || e.currentTarget === e.target) {
-      onClose();
-    }
-  };
+  const handleKeyDown = useCallback(
+    e => {
+      if (e.code === 'Escape' || e.currentTarget === e.target) {
+        onClose();
+      }
+    },
+    [onClose]
+  );
 
   useEffect(() => {
     document.addEventListener('keydown', handleKeyDown);
     return () => {
       document.removeEventListener('keydown', handleKeyDown);
     };
-    // eslint-disable-next-line
-  }, []);
+  }, [handleKeyDown]);
 
   return createPortal(
     <ModalBackdrop onClick={handleKeyDown}>
